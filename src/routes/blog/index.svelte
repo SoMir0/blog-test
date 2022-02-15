3c869415ffeb1c51badd6c25673ecc7c3e640263
SoMir0 <mirsenkahrovic@gmail.com>
2022-02-15 16:01:08 +0100

    Blog styling

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -4,12 +4,28 @@
 
 <main>
     <Header />
-    <p>Blog</p>
+    {#each [0, 1, 2, 3] as cat}
+        <article>
+            <h2>Title</h2>
+            <hr>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat aspernatur eveniet aliquid rem ratione laboriosam quis, illum quaerat, repellendus aut quae ea, iste ad. Fuga commodi expedita eveniet eos molestias!</p>
+        </article>
+    {/each}
 </main>
 
 <style lang="scss">
     :global(body), main {
         background: #3d3f43;
         color: #eee;
+    }
+
+    article {
+        display: inline-block;
+        max-width: 60vw;
+        padding: 1rem;
+        margin: 1rem;
+        margin-left: 5rem;
+        border-radius: 25px;
+        background: #2c2e32;
     }
 </style>
